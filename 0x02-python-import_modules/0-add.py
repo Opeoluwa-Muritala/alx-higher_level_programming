3b6993da3745eb1452b65c2b0ae5d0d7b1eee283
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 17:56:49 +0100

    Import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     from add_0 import add
 
     a = 1
-    b = 2
-
-    c = add(a, b)
-    print(f"{a} + {b} = {c}")
+    b = 1
+    
+    print("{} + {} = {}".format(a, b, add(a, b)))
