6201667eee4c95ab494dc02da62ae869913a2f13
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 21:56:12 +0100

    Infinite Addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-if nam
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    c = len(sys.argv)
+    for i in range(1, c):
+        sum += int(sys.argv[i])
+    print(sum)
+
