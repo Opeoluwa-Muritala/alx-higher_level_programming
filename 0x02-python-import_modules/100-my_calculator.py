bef85955b92de31de4d55f3081c72c042a9a9406
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 22:53:21 +0100

    Import end for now

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if (argv - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
