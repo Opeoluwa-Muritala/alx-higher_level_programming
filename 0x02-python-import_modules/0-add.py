92e7638bbafa541a9aa4948255dc8e0fd51cb90e
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 17:54:49 +0100

    Import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
 
-c = add(a, b)
-print( f"{a} + {b} = {c}" )
+    a = 1
+    b = 2
+
+    c = add(a, b)
+    print(f"{a} + {b} = {c}")
