2c340df6b5c53ffab15ec54fcc83a905e1249783
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 17:09:35 +0100

    fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 word = str.split()
-print(f"{word[5]}-{word[6]} {word[7]} {word[12]} {word[0]}")
+print("{} {}{}{} {}".format(word[5],word[6],word[7],word[12],word[0],word[8]))
