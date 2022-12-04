33ced9696f55a827c8acafe913e606c858a0234f
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 21:37:53 +0100

    Import sys

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def no_of_argc(*argv):
-    c = len(argv)
-    argv = str(argv)
-    lis = list(argv.split())
-    for i in range(0, c + 1):
-        for li in lis:
-            if i == 0:
-                print(":")
-            else:
-                print(f"{i}: {li}\n")
-no_of_argc(*argc)
+import sys
+
+argv = len(sys.argv)
+
+for i in range(1, argv):
+    print("{}: {}".format(i, sys.argv[i]))
