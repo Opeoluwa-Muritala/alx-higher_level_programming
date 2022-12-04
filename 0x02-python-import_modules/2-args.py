ba3f532ab9b22a291e87295e6cf76fa4eb8214d2
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2022-12-04 21:49:33 +0100

    Import issue fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     import sys
 
     argv = len(sys.argv)
-
+    if (argv - 1) == 0:
+        print("{} arguments.".format(argv - 1))
+    elif (argv - 1) == 1:
+        print("{} argument:".format(argv - 1))
+    elif (argv - 1) > 1:
+        print("{} arguments:".format(argv - 1))
     for i in range(1, argv):
         print("{}: {}".format(i, sys.argv[i]))
