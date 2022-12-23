ce94fee050303c012644d416b84a2ca08ac068b5
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2022-12-23 21:46:43 +0100

    second error fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    lis = []
     for i in [matrix[0], matrix[1]:
-            print(f"{matrix**2}")
+            lis.append([i**2] for i in matrix.split())
