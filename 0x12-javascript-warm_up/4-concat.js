78977c11b89c843e7557c4d30a2d0073f562788c
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-03-14 00:16:58 +0100

    Create

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+args = process.argv
+args.forEach((val,idx) => {
+  if (idx === 2) {
+    string1 = val;
+    }
+  if (idx === 3) {
+    string2 = val;
+    }
+  });
+
+string = string1.concat(" is ", string2);
