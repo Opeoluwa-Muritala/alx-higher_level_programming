230a43ccd40221fa5d6debf82941be6ccc5ce577
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-03-14 23:34:06 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 argv = process.argv;
-if (argv.length ===3){
+if (argv.length === 2){
+console.log("No Argument");
+}
+else if (argv.length === 3){
 console.log("Argument found");
 }
-else if (argv.length > 3){
-console.log("Arguments found");
-}
 else{
-console.log("No Argument");
+console.log("Arguments found");
 }
