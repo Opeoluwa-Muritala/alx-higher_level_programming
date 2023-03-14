bdb60f1f0127d9581b96219641baa7d41256a5bb
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-03-14 23:40:54 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 argv = process.argv;
-
-argv.forEach((val, index) => {
-  if (index <= 1) {
-    console.log('No Argument');
-  } else if (index >= 2) {
-    console.log('${val}');
-  }
-});
+if (argv[2] === undefined){
+  console.log("No Argument");
+ } else {
+   console.log(argv[2]);
