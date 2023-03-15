a7598089d4db52df55bdd19b1fef2addb2b7d969
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-03-15 15:56:24 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,10 +4,12 @@ args = process.argv;
 if (args.length === 4) {
   string1 = args[2];
   string2 = args[3];
-}
-else if (args.length === 3) {
- string1 = args[2]; 
- string2 = undefined;
+} else if (args.length === 3) {
+  string1 = args[2];
+  string2 = undefined;
+} else {
+	string1 = undefined;
+	string2 = undefined;
 }
 
 string = string1.concat(' is ', string2);
