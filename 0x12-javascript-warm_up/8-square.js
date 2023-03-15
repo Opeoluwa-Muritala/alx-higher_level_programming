70246c9ad2e7c09b05849f95eca1e915c17e00ea
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-03-16 00:37:07 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (argv[2] === undefined || isNaN(argv[2]) === true){
 } else if (argv.length === 3) {
   for (let i = 0; i < argv[2]; i++) {
     for (let j = 0; j < argv[2]; j++) {
-     string += 'X';
+     string += 'x';
     }
    string += '\n';
   }
