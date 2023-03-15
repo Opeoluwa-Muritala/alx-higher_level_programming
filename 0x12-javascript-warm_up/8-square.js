9f2e974ee814471f479b5655a63e0c98a4e8ed0c
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-03-16 00:38:45 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const argv = process.argv;
-string = '';
+let string = '';
 
-if (argv[2] === undefined || isNaN(argv[2]) === true){
+if (argv[2] === undefined || isNaN(argv[2]) === true) {
   console.log('Missing size');
 } else if (argv.length === 3) {
   for (let i = 0; i < argv[2]; i++) {
     for (let j = 0; j < argv[2]; j++) {
-     string += 'x';
+      string += 'x';
     }
-   string += '\n';
+    string += '\n';
   }
-} 
+}
 
 console.log(string);
