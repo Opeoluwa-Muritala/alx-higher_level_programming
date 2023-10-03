5f47d318a1838a6b47fad7e676e6b5a24e7b70e5
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-10-03 12:51:06 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for number in range(1, 101):
         if (number % 3 and number % 5):
-            print("FizzBuzz"), end=' ')
+            print("FizzBuzz", end=' ')
         elif (number % 3):
-            print("Fizz"), end=' ')
+            print("Fizz", end=' ')
         elif (number % 5):
-            print("Buzz"), end=' ')
+            print("Buzz", end=' ')
         else:
             print("number", end=' ')
 
