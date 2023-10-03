3ae4d1f111f5cec0839185e1f94d9206eb45d43a
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-03 12:48:26 +0100

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3 and number % 5):
+            print("FizzBuzz"), end=' ')
+        elif (number % 3):
+            print("Fizz"), end=' ')
+        elif (number % 5):
+            print("Buzz"), end=' ')
+        else:
+            print("number", end=' ')
+
