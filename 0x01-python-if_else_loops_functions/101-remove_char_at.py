f0e7315b799b312ad9f784a1c27a8a0132bdd804
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-04 02:23:37 +0100

    Bytecode two

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n < len(str):
-        str = str.replace(str[n],'')
+    if 0 < n < len(str):
+        str = str.replace(str[n], '')
     else:
         str = str
     return str
