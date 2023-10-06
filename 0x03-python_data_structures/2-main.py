454782a50f2430fc7e04640e43b87ae5d1f11d98
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-06 08:56:57 +0100

    Test and Fix Complete

--- 0x03-python_data_structures/2-main.py
+++ 0x03-python_data_structures/2-main.py
@@ -2,7 +2,7 @@
 replace = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
-idx = 30
+idx = 3
 new_element = 9
 new_list = replace(my_list, idx, new_element)
 print(my_list)
