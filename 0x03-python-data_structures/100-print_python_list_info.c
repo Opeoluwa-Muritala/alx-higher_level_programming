68e1601faa7a5244ec4f0b6e3cbfb75b132eb809
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-07 20:51:47 +0100

    CPython Fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,17 +4,24 @@
  * print_python_list_info - Prints basic info about Python lists.
  * @p: A PyObject list.
  */
-void print_python_list_info(PyObject *p){
-	if (PyList_Check(p)){
+void print_python_list_info(PyObject *p)
+{
+	if (PyList_Check(p))
+	{
 		Py_ssize_t size = PyList_Size(p);
 		Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
 		printf("[*] Size of the Python List = %d\n", size);
 		printf("[*] Allocated = %d\n", alloc);
-		for (i = 0; i < size; i++){
+		for (i = 0; i < size; i++)
+		{
 		printf("Element %d: ", i);
 		obj = PyList_GetItem(p, i);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
-	}else{
+		}
+	}
+	else
+	{
 		fprintf(stderr, "Invalid Python List\n");
 	}
 }
