5feaea6f2788ffd05ff3cca0abb1f3ba07fd0126
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-07 20:06:06 +0100

    Code Fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{}".format(num), end="")
-            if num != row[-1]:
-                print(" ", end="")
-        print("")
+def delete_at(my_list=[], idx=0):
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
