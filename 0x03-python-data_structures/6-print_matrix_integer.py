4c781534ca10c38eddb4cdcc8e8eb559ac210196
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-07 20:14:41 +0100

    Update to 6-print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
-    return (my_list)
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end="")
+            if (num != row[-1]):
+                print(" ", end="")
+        print("")
