f4271a030915b8ca243213d55bbce940b81b229d
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-12 12:01:20 +0100

    More on Python

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """t = tuple, w = weight, e = element"""
+    if len(my_list) == 0:
+        return 0
+    bottom = 0
+    top = 0
+    for t in my_list:
+        bottom += t[1]
+        top += t[0] * t[1]
+    average = top/bottom
+    return average
