86b8e711d16916f8c250ab31ffe39da737d5a882
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2023-10-12 11:28:05 +0100

    More On Data Structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    key = list(a_dictionary.keys())
+    max_age = a_dictionary[key]
+    for key, age in a_dictionary.items():
+        if age > max_age:
+            max_age = age
+            key = key
+    return (key)
