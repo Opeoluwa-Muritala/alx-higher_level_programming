339740cdddc51181d082f776f0285964b4c735cc
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-09 15:39:49 -0500

    7-base_geometry.py pycodestyle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """Baseclass Geometery Continuation"""
 
+
 class BaseGeometry():
     """Class to perform non implemented basic operations"""
+
     def area(self):
         """Unimplemented Class To Check area"""
+
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Check if the value is an integer """
-        if type(value) !=  int:
+
+        if type(value) != int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
