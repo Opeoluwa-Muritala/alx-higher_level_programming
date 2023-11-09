5d8eb132bbce5237424d009ffa57c754cd43645a
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-09 08:47:19 -0500

    2-is_same_class pycodestyle fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A function that returns True if the object is 
+"""A function that returns True if the object is
 exactly an instance of the specified class ; otherwise False"""
 
 
@@ -9,4 +9,4 @@ def is_same_class(obj, a_class):
         return True
     else:
         return False
-    #return isinstance(obj, a_class)
+# return isinstance(obj, a_class)
