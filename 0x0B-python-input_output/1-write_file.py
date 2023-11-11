d664b5db6c30eea0ed4c1b2fad0699746fea1bfe
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-11 11:57:38 -0500

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write into an external File"""
+
+
+def write_file(filename="", text=""):
+    """Write A File"""
+    with open(filename, 'w') as file:
+        file.write(text)
