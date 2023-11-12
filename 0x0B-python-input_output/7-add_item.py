914044088e65d718104312b08d5471ec3583f36b
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-11 21:31:32 -0500

    7-add_item.py get argument from sys

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that adds all arguments to a Python list, and then save them to a file"""
-import json
+import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file= __import__('6-load_from_json_file').load_from_json_file
@@ -8,8 +8,9 @@ load_from_json_file= __import__('6-load_from_json_file').load_from_json_file
 
 def add_item(item, filename = "add_item.json"):
     """Create A list in a JSON file and Append New Items to It"""
-    argu = []
-    argu.append
-    jsonFile = load_from_json_file(filename)
-    jsonFile.extend(argu)
-    save_to_json_file(argu, filename)
+    try:
+        jsonItems = load_from_json_file(filename)
+    except FileNotFoundError:
+        jsonItems = []
+    jsonItems.extend(sys.argv[1:])
+    save_to_json_file(jsonItems, filename)
