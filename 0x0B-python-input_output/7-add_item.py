339c9c1eef20cbb6c06bc5de22880294fcead300
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-11 21:20:12 -0500

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that adds all arguments to a Python list, and then save them to a file"""
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file= __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item(item, filename = "add_item.json"):
+    """Create A list in a JSON file and Append New Items to It"""
+    save_to_json_file([], filename)
+    jsonFile = load_from_json_file(filename)
+    jsonFile.append(item)
+    save_to_json_file(item, filename)
