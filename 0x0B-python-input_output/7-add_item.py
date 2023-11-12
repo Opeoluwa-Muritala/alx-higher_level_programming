f2497bae55c17cfac242bc08c9ebc8560d98a545
Opeoluwa-Muritala <muritalaopeoluwa10@gmail.com>
2023-11-11 21:36:10 -0500

    7-add_item.py tests passed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,11 +6,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file= __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(item, filename = "add_item.json"):
-    """Create A list in a JSON file and Append New Items to It"""
-    try:
-        jsonItems = load_from_json_file(filename)
-    except FileNotFoundError:
-        jsonItems = []
-    jsonItems.extend(sys.argv[1:])
-    save_to_json_file(jsonItems, filename)
+"""Create A list in a JSON file and Append New Items to It"""
+filename = "add_item.json"
+try:
+    jsonItems = load_from_json_file(filename)
+except FileNotFoundError:
+    jsonItems = []
+jsonItems.extend(sys.argv[1:])
+save_to_json_file(jsonItems, filename)
